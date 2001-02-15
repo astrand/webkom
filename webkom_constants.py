11c120326673be21f57274dc2d158935fba9009c
Peter Åstrand <astrand@lysator.liu.se>
2001-02-15 20:46:40 +0000

    Constants fixes.

--- webkom_constants.py
+++ webkom_constants.py
@@ -1,6 +1,4 @@
 
-from HTMLcolors import *
-
 LOG_DIR = "/home/astrand/webkom/logs/"
 MODULES_PATH = "/home/astrand/webkom/python-modules/"
 VERSION = "0.4"
@@ -10,7 +8,7 @@ MAX_SUBJ_PER_PAGE = 25
 MAX_CONFS_PER_PAGE = 15
 MAINTAINER_NAME = "Peter Åstrand"
 MAINTAINER_MAIL = "astrand@lysator.liu.se"
-INACTIVE_LINK_COLOR = GREY6
+
 MAX_CONFERENCE_LEN = 37
 # 30 minutes auto-logout
 SESSION_TIMEOUT = 1800
