4c0e889c1126f49bc12eeef3ffbb97c881f369ed
Peter Åstrand <astrand@lysator.liu.se>
2001-03-03 13:55:06 +0000

    Adopted class to match WebKOM. Fixed a memory leak.

--- sz_fcgi.py
+++ sz_fcgi.py
@@ -1,7 +1,13 @@
 # sz_fcgi.py - Multithreaded FastCGI Wrapper
-__version__ 	= "v0.8  19/10/1998 ajung"
+#__version__ 	= "v0.8  19/10/1998 ajung"
+__version__ 	= "v0.8  19/10/1998 ajung (WebKOM patched)"
 __doc__      	= "Multithreaded FastCGI Wrapper"
-
+#
+# Changed 2001-03-03 by Peter Åstrand <astrand@lysator.liu.se>:
+# Deleting request after Finish, to prevent memory leak.
+# Changed formatting to match WebKOM standards.
+# Changed SZ_FCGI.pr to behave more like a normal fcgi.out.write. 
+#
 
 import sys
 import thread
@@ -10,50 +16,47 @@ from fcgi import *
 
 
 class SZ_FCGI:
+    # Constructor
+    def __init__(self, func):
+        self.func = func
+        self.handles  = {}
+        return None
 
-	# Constructor
-	def __init__(self,func):
-		self.func = func
-		self.handles  = {}
-		return None
+    # create a new thread to handle requests
+    def run(self):
+	while isFCGI():
+	    req = FCGI()
+	    thread.start_new_thread(self.handle_request, (req,0))
 
-	# create a new thread to handle requests
-	def run(self):
-		while isFCGI():
-			req = FCGI()
-			thread.start_new_thread(self.handle_request,(req,0))
-			
-	# Finish thread and send all data back to the FCGI parent
-	def finish(self):
-		req  = self.handles[thread.get_ident()]
-		req.Finish()
-		thread.exit()	
-		
+    # Finish thread and send all data back to the FCGI parent
+    def finish(self):
+	th_id = thread.get_ident()
+	req  = self.handles[th_id]
+	# Note: This is thread-safe, since req still references the instance.
+	# If it wouldn't, req.__del__ might be called and it that case
+	# we would need a lock. 
+	del self.handles[th_id]
+	req.Finish()
+	del req
+	thread.exit()	
 
+    # Call function - handled by a thread
+    def handle_request(self, *args):
+	req = args[0]
+	self.handles[thread.get_ident()] = req
 
-	# Call function - handled by a thread
-	def handle_request(self,*args):
+	try:
+	    self.func(self, req.env, req.getFieldStorage())
+	except:
+	    pass
 
-		req = args[0]
-		self.handles[thread.get_ident()] = req
+    # Our own FCGI print routine
+    def pr(self, out_string):
+	req = self.handles[thread.get_ident()]
 
-		try:
-			self.func(self,req.env,req.getFieldStorage())
-		except:
-			pass
-
-
-	# Our own FCGI print routine
-	def pr(self,*args):
-	
-		req = self.handles[thread.get_ident()]
-
-		try:
-			s=''
-			for i in args: s=s+str(i)
-			req.out.write(s+'\n')
-			req.out.flush()
-		except:
-			pass
-
+	try:
+	    req.out.write(out_string)
+	    req.out.flush()
+	except:
+	    pass
 
