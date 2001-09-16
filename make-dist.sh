b8f5cd43194306df0e312873f2e78add07567bab
Peter Åstrand <astrand@lysator.liu.se>
2001-09-16 20:19:55 +0000

    added make-dist.sh

---
+++ make-dist.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+VERSION=`python -c "from webkom_constants import *; print VERSION"`
+PWD=`pwd`
+SRCDIR=`basename ${PWD}`
+./set_version.py
+(cd po; ./install.py ../locale)
+find . -name '*~' -exec rm \{\} \;
+find . -name '*.pyc' -exec rm \{\} \;
+find . -name '*.pyo' -exec rm \{\} \;
+find logs -mindepth 1 -and -not -name README -exec rm -f \{\} \;
+cd ..
+rm -f webkom-${VERSION}.tgz
+DISTFILES=`find ${SRCDIR} \
+ -not -path '*CVS*' -and -not -name .cvsignore -and -not -path .\
+ -type f`
+tar zcvf webkom-${VERSION}.tgz ${DISTFILES}
