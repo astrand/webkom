80519a1cab6d8221a5a797587817afe253a92933
Peter Åstrand <astrand@lysator.liu.se>
2001-11-17 23:49:03 +0000

    Removed silly finalizer. Up-to-sync with latest python-lyskom version.

--- kom.py
+++ kom.py
@@ -6,7 +6,6 @@ import socket
 import time
 import string
 import select
-import sys
 
 #
 # Constants
@@ -1088,7 +1087,7 @@ class Time:
             self.minutes = 0
             self.hours = 0 
             self.day = 0
-            self.month = 0 # 1 ... 12
+            self.month = 0 # 0 .. 11 
             self.year = 0 # no of years since 1900
             self.day_of_week = 0 # 0 = Sunday ... 6 = Saturday
             self.day_of_year = 0 # 0 ... 365
@@ -1744,7 +1743,6 @@ class Connection:
     # INITIALIZATION ETC.
 
     def __init__(self, host, port = 4894, user = ""):
-
         # Create socket and connect
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host, port))
@@ -2060,18 +2058,6 @@ class CachedConnection(Connection):
         self.add_async_handler(ASYNC_NEW_RECIPIENT, self.cah_new_recipient)
         self.add_async_handler(ASYNC_SUB_RECIPIENT, self.cah_sub_recipient)
         self.add_async_handler(ASYNC_NEW_MEMBERSHIP, self.cah_new_membership)
-
-
-    def __del__(self):
-        print "Destroyed!"
-
-    def destroy(self):
-        del self.uconferences
-        del self.conferences
-        del self.persons
-        del self.textstats
-        del self.subjects
-        del self.async_handlers
 
     # Fetching functions (internal use)
     def fetch_uconference(self, no):
