1a188b0e9799a7b43136cfe134de767f8aadcb56
Peter Åstrand <astrand@lysator.liu.se>
2001-12-02 21:25:25 +0000

    Added --no-location.

--- po/upd-sv.sh
+++ po/upd-sv.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-../utils/pygettext.py -d webkom ../webkom.py ../webkom_utils.py
+../utils/pygettext.py --no-location -d webkom ../webkom.py ../webkom_utils.py
 mv sv.po sv.po.old
 msgmerge sv.po.old webkom.pot > sv.po
