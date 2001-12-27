775ebe1d8726cb2793e1e3963f2f6243d844d117
Peter Åstrand <astrand@lysator.liu.se>
2001-12-27 21:37:06 +0000

    Simplified console. I gave up readline support. Maybe some other time...

--- utils/console.py
+++ utils/console.py
@@ -18,55 +18,27 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. 
 
-import readline
 import sys
 import socket
 import select
-import termios
-
-
-try:
-    str(sys.ps1)
-except:
-    sys.ps1 = ">>> "
-
-
-def write_prompt():
-    sys.stdout.write(sys.ps1)
-    sys.stdout.flush()
-
 
 def interact(sockname):
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     sock.connect(sockname)
 
-    old = termios.tcgetattr(sys.stdin)
-    new = termios.tcgetattr(sys.stdin)
-    termios.tcsetattr(sys.stdin, termios.ICANON, new)    
-
-    try:
-        write_prompt()
-        while 1:
-            [x, y, z] = select.select([sock, sys.stdin], [], [])
-            obj = x[0]
-            if obj == sock:
-                data = sock.recv(1)
-                if not data:
-                    raise "Closed connection"
-                sys.stdout.write(data)
-                sys.stdout.flush()
-            elif obj == sys.stdin:
-                # Remove duplicate first char. 
-                # FIXME: Why is this necessary?
-                sys.stdout.write("\x08")
-                # Remove prompt from server. 
-                sys.stdout.write("\x08" * 4)
-                line = raw_input(sys.ps1)
-                line += "\n"
-                sock.send(line)
-    finally:
-        termios.tcsetattr(sys.stdin, termios.ICANON, old)
-
+    while 1:
+        [x, y, z] = select.select([sock, sys.stdin], [], [])
+        obj = x[0]
+        if obj == sock:
+            data = sock.recv(1)
+            if not data:
+                raise "Closed connection"
+            sys.stdout.write(data)
+            sys.stdout.flush()
+        elif obj == sys.stdin:
+            line = raw_input()
+            line += "\n"
+            sock.send(line)
 
 if __name__=="__main__":
     if len(sys.argv) < 2:
